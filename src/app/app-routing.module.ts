34b9b49887dee768f649b628a85d5111401a9539
candeloronat <nataliacandeloro@gmail.com>
2024-01-11 12:27:45 -0300

    Ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', //TODO: localHost:4200/auth
+    loadChildren: () => import('./module/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'admin', //TODO: localHost:4200/admin
+    loadChildren: () => import('./module/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'patient', //TODO: localHost:4200/company
+    loadChildren: () => import('./module/patient/patient.module').then(m => m.PatientModule)
+  },
+  {
+    path: '**', //TODO: No existe te envio a la ruta arriba login
+    redirectTo: 'auth',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
